47c6eb8c943c96382af7bd14e242bebe54caaa04
hxxtae <fkdlxmfkdl@gmail.com>
2022-03-14 17:24:23 +0900

    Updates

--- src/atoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const sliderLeave = atom({
-  key: "sliderLeave",
-  default: false
-});
--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-const IMAGE_PATH = "https://image.tmdb.org/t/p";
-
-export function makeImagePath(id: string, format: string = 'original') {
-  return `${IMAGE_PATH}/${format}${id}`;
-};
