de6707cf7ebfb44ba7240c8f42ffa582c155dfe5
hxxtae <fkdlxmfkdl@gmail.com>
2022-10-03 17:10:44 +0900

    :books: [Docs]: Update directory of constants
    - Create file of index.ts
    - Add object of queryKey in constants.ts
    - move theme file into constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,3 +5,17 @@ export enum SliderCategory {
   "Upcoming",
   "onAir",
 }
+
+export const queryKey = {
+  movie: {
+    all: ['movies'] as const,
+    nowPlaying: () => [...queryKey.movie.all, 'nowPlaying'] as const,
+    popular: () => [...queryKey.movie.all, 'popular'] as const,
+    top: () => [...queryKey.movie.all, 'top'] as const,
+    upcoming: () => [...queryKey.movie.all, 'upcoming'] as const,
+  },
+  tv: {
+    all: ['Tv'] as const,
+    onAir: () => [...queryKey.tv.all, 'onAir'] as const,
+  }
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export { boxVariants, infoVariants, loadVariants, navVariants, slideVariants } from './animation';
+export { SliderCategory, queryKey } from './constants';
+export { darkTheme } from './theme';
