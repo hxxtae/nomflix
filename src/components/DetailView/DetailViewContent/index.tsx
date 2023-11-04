26f06d8871d6cf961aee846812c569dcee79395a
hxxtae <fkdlxmfkdl@gmail.com>
2023-11-04 23:48:08 +0900

    :book: [feat]: Add DetailViewContent.tsx
    - add new component of "DetailViewContent" & style.ts

---
+++ src/components/DetailView/DetailViewContent/index.tsx
@@ -0,0 +1,74 @@
+import { dto } from '../../../apis';
+import { formatImagePath, toTime } from '../../../utils';
+import DetailViewBtn from './DetailViewBtn';
+import * as S from './style';
+
+interface IDetailViewContent {
+  detailData?: dto.IContentDetailsData;
+  similarData?: dto.IContentData[];
+}
+
+function DetailViewContent({ detailData, similarData }: IDetailViewContent) {
+  const showNumData = (num: number = 0) => {
+    return parseInt(num.toString(), 10);
+  }
+
+  const showStrData = (data: unknown): string => {
+    if (typeof data === 'string' && data) return data;
+    if (typeof data === 'number') return data.toString();
+
+    return '-';
+  }
+
+  const showArrData = <T extends dto.ICompany | dto.IGenres>(arr?: T[]) => {
+    if (Array.isArray(arr) && arr.length > 0) {
+      return arr.map(item => (<span key={item.id}>{item.name}</span>))
+    };
+
+    return '-';
+  }
+
+  return (
+    <S.Content>
+      <S.Title>{detailData?.title || detailData?.name}</S.Title>
+      <DetailViewBtn popularity={showNumData(detailData?.popularity)} />
+      <S.Overview>{showStrData(detailData?.overview)}</S.Overview>
+
+      <S.SubTitle>Similar Contents</S.SubTitle>
+      <S.ImageBox>
+        {similarData?.slice(0, 9).map(data => (
+          data.poster_path ?
+          <img key={data.id} src={formatImagePath(data.poster_path, 'w200')} alt={data.poster_path} /> : null
+        ))}
+      </S.ImageBox>
+
+      <S.SubTitle>{showStrData(detailData?.title ?? detailData?.name)} Information</S.SubTitle>
+      <S.Info>
+        <strong>Productions: </strong>
+        {showArrData(detailData?.production_companies)}
+      </S.Info>
+      <S.Info>
+        <strong>Genres: </strong>
+        {showArrData(detailData?.genres)}
+      </S.Info>
+      <S.Info>
+        <strong>Original Name: </strong>
+        <span>{showStrData(detailData?.original_title ?? detailData?.original_name)}</span>
+      </S.Info>
+      <S.Info>
+        <strong>Release Date: </strong>
+        <span>{showStrData(detailData?.release_date ?? detailData?.first_air_date)}</span>
+      </S.Info>
+      <S.Info>
+        <strong>Run Time: </strong>
+        <span>{toTime(detailData?.runtime)}</span>
+      </S.Info>
+      <S.Info>
+        <strong>Status: </strong>
+        <span>{showStrData(detailData?.status)}</span>
+      </S.Info>
+    </S.Content>
+  )
+}
+
+export default DetailViewContent;
---
+++ src/components/DetailView/DetailViewContent/style.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { media } from '../../../utils';
+
+export const Content = styled.div`
+  position: relative;
+  top: -80px;
+  padding: 0 50px;
+`;
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.textColor};
+  padding: 20px 0px;
+  font-size: 46px;
+`;
+
+export const Overview = styled.p`
+  padding: 20px 0px;
+  color: ${({ theme }) => theme.textColor};
+  letter-spacing: 1px;
+  line-height: 30px;
+  margin-bottom: 60px;
+`;
+
+export const SubTitle = styled.h2`
+  color: ${({ theme }) => theme.textColor};
+  padding: 20px 0px;
+  margin-bottom: 20px;
+  font-size: 26px;
+`;
+
+export const ImageBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 60px;
+
+  img {
+    border-radius: 7px;
+  }
+  
+  ${media('mobile')} {
+    img {
+      max-width: 150px;
+      width: 100%;
+    }
+  }
+`;
+
+export const Info = styled.div`
+  margin-bottom: 8px;
+  line-height: 1.3em;
+
+  strong {
+    color: ${({ theme }) => theme.thirdColor};
+  }
+
+  span {
+    margin-bottom: 20px;
+  }
+
+  span:after {
+    content: ', '
+  }
+
+  span:last-child:after {
+    content: ''
+  }
+`;
