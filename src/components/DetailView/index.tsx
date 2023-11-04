b334a9e56f06b803bc0a923183878ddc9e025931
hxxtae <fkdlxmfkdl@gmail.com>
2023-11-04 23:37:15 +0900

    :books: [Docs]: Update DetailView Component
    - update fetch hook of "useDetailDataFetch"
    - add fetch hoos of "useContentFetch"
    - refactoring code...

--- src/components/DetailView/index.tsx
+++ src/components/DetailView/index.tsx
@@ -1,68 +1,42 @@
-import { faPlay, faThumbsDown, faThumbsUp, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { formatImagePath } from '../../utils';
 import { api, dto, query } from '../../apis';
 import { queryKey } from '../../constants';
+import DetailViewContent from './DetailViewContent';
 import * as S from './style';
 
 interface IDetailView {
-  data: dto.IData;
+  data: dto.IContentData;
   kind: number;
   closeDetail: () => void;
 };
 
 function DetailView({ data, kind, closeDetail }: IDetailView) {
-  const { isLoading, data: detailData } = query.useDetailDataFetch(queryKey.detail.all, () => api.getDetail(data.id + "", kind));
+  const { isLoading: isDetailLoading, data: detailData } = query.useContentDetailFetch(queryKey.detail.all, () => api.getDetail(data.id.toString(), kind));
+  const queryKeyStata = kind < 20 ?
+    queryKey.movie.similar(data.id.toString()) :
+    queryKey.tv.similar(data.id.toString());
+  const queryFuncState = kind < 20 ?
+    () => api.getMovieSimilarAll(data.id.toString()) :
+    () => api.getTvSimilarAll(data.id.toString());
+  const { isLoading: isSimilarLoading, datas: similarData } = query.useContentFetch(queryKeyStata, queryFuncState);
+
+  const isLoading = isDetailLoading || isSimilarLoading;
   
   return (
     <S.Wrapper>
-        <S.Section
-          layoutId={data.id + kind.toString()}>
-          <S.Image bgphoto={formatImagePath(data.backdrop_path)} />
-          <S.Content>
-            <S.Title>{data.title || data.name}</S.Title>
-            <S.ButtonGroup>
-              <button>
-                <span>재생</span>
-                <FontAwesomeIcon icon={faPlay} size="1x" />
-              </button>
-              <button>
-                <FontAwesomeIcon icon={faThumbsUp} size="1x" />
-              </button>
-              <button>
-                <FontAwesomeIcon icon={faThumbsDown} size="1x" />
-              </button>
-              <button>
-                <FontAwesomeIcon icon={faPlus} size="1x" />
-              </button>
-            </S.ButtonGroup>
-            <S.Overview>{data.overview}</S.Overview>
-            {isLoading ||
-              <>
-                <S.Production>
-                  {detailData?.production_companies.map((item, index) => (
-                    <div key={index.toString()}>
-                      <img src={formatImagePath(item.logo_path, 'w500')} alt={item.name} />
-                    </div>
-                  ))}
-                </S.Production>
-                <S.Production>
-                  {detailData?.production_companies.map((item, index) => (
-                    <span key={index.toString()}>{item.name}</span>
-                  ))}
-                </S.Production>
-              </>
-            }
-          </S.Content>
-        </S.Section>
-        <S.Overlay
-          onClick={closeDetail}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}>
-        </S.Overlay>
-      </S.Wrapper>
+      <S.Section
+        layoutId={data.id + kind.toString()}>
+        <S.Image bgphoto={formatImagePath(data.backdrop_path)} />
+        {isLoading || <DetailViewContent detailData={detailData} similarData={similarData} />}
+      </S.Section>
+      
+      <S.Overlay
+        onClick={closeDetail}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}>
+      </S.Overlay>
+    </S.Wrapper>
   )
 }
 
--- src/components/DetailView/style.ts
+++ src/components/DetailView/style.ts
@@ -1,5 +1,7 @@
-import { motion } from 'framer-motion';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+import { media } from '../../utils';
 
 export const Wrapper = styled.div`
   position: fixed;
@@ -21,13 +23,18 @@ export const Overlay = styled(motion.div)`
 export const Section = styled(motion.section)`
   position: absolute;
   bottom: 0;
-  width: 42vw;
+  max-width: 816px;
+  width: 100%;
   height: 95vh;
   background-color: ${props => props.theme.bgColor};
   border-radius: 10px;
   overflow: auto;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
   z-index: 1;
+
+  ${media('mobile')} {
+    max-width: 616px;
+  }
 `;
 
 export const Image = styled(motion.div)<{bgphoto: string}>`
@@ -37,83 +44,4 @@ export const Image = styled(motion.div)<{bgphoto: string}>`
   background-size: cover;
   background-position: center center;
   background-repeat: no-repeat;
-`;
-
-export const Content = styled.div`
-  position: relative;
-  top: -80px;
-  padding: 0 40px;
-`;
-
-export const Title = styled.h3`
-  color: ${(props) => props.theme.textColor};
-  padding: 20px;
-  font-size: 46px;
-`;
-
-export const ButtonGroup = styled.div`
-  display: inline-flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 20px;
-
-  button {
-    width: 40px;
-    height: 40px;
-    border: 2px solid rgba(80, 80, 80, 1);
-    border-radius: 50%;
-    background-color: ${props => props.theme.bgColor};
-    margin-right: 10px;
-    color: rgba(255, 255, 255, .8);
-    cursor: pointer;
-
-    &:active {
-      opacity: .8;
-    }
-
-    &:first-child {
-      width: 150px;
-      background-color: rgba(255, 255, 255, 1);
-      border-radius: 10px;
-      margin-right: 20px;
-      font-size: 20px;
-      font-weight: bold;
-      border: none;
-      color: rgba(0, 0, 0, .8);
-
-      span {
-        margin-right: 22px;
-      }
-    }
-  }
-`;
-
-export const Overview = styled.p`
-  padding: 20px;
-  color: ${(props) => props.theme.textColor};
-  letter-spacing: 1px;
-  line-height: 30px;
-  margin-bottom: 100px;
-`;
-
-export const Production = styled.div`
-  padding: 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-  flex-wrap: wrap;
-
-  div {
-    width: 120px;
-    margin-bottom: 20px;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
-  span {
-    margin-bottom: 20px;
-  }
 `;
