2d4ec3fd78b7b50e01cd8a750584eb5eca29060a
hxxtae <fkdlxmfkdl@gmail.com>
2023-11-07 15:35:18 +0900

    :books: [Docs]: Update index of components
    - add component of "Banner"
    - add component of "Slider"
    #Create Banner
    #Create Slider

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
-export { default as DetailView } from './DetailView';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
-export { default as Search } from './Search';
 export { default as Loading } from './Loading';
-export { default as SliderContent } from './SliderContent';
+export { default as Banner } from './Banner';
+export { default as Slider } from './Slider';
+export { default as DetailView } from './DetailView';
+export { default as Search } from './Search';
 
