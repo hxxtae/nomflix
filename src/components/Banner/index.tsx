9ccf7f91facbf0d40293cb714014ca98c919cc69
hxxtae <fkdlxmfkdl@gmail.com>
2023-11-07 15:26:41 +0900

    :book: [feat]: Create Component of "Banner"

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,57 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useRecoilValue } from 'recoil';
+import { useState, memo } from 'react'
+
+import { atomOfContentData } from '../../global'
+import { formatImagePath } from '../../utils';
+import * as S from './style';
+import Loading from '../Loading';
+import PortalModal from '../PortalModal';
+import DetailView from '../DetailView';
+import { MovieCategory, TvCategory } from '../../constants';
+
+interface IBanner {
+  kind: MovieCategory | TvCategory;
+}
+
+function Banner({ kind }: IBanner) {
+  console.log('banner')
+  const contentData = useRecoilValue(atomOfContentData);
+  const [detailState, setDetailState] = useState(false);
+
+  const showDetail = () => {
+    if (!contentData?.id) return;
+    setDetailState(true);
+  }
+
+  const closeDetail = () => {
+    setDetailState(false);
+  }
+
+  return (
+    <>
+      {contentData.id ?
+        <S.Banner bgphoto={formatImagePath(contentData.backdrop_path)}>
+          <S.Title>{contentData.title}</S.Title>
+          <S.ButtonWrapper>
+            <S.BannerButton>
+              <FontAwesomeIcon icon={faPlay} /><span>재생</span>
+            </S.BannerButton>
+            <S.BannerButton onClick={showDetail}>
+              <FontAwesomeIcon icon={faPlus} /><span>상세 정보</span>
+            </S.BannerButton>
+          </S.ButtonWrapper>
+          <S.Overview>{contentData.overview}</S.Overview>
+        </S.Banner>
+        : <Loading />}
+      
+      {detailState && 
+        <PortalModal>
+          <DetailView kind={kind} data={contentData} closeDetail={closeDetail} />
+        </PortalModal>}
+    </>
+  )
+}
+
+export default memo(Banner);
---
+++ src/components/Banner/style.ts
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div<{bgphoto: string}>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 60px;
+  background-image: linear-gradient(rgba(20, 20, 20, 0), rgba(20, 20, 20, 1)), url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 68px;
+  font-weight: bold;
+  margin-bottom: 40px;
+
+  &::selection {
+    background-color: transparent;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
+export const BannerButton= styled.button`
+  border-radius: 5px;
+  border: none;
+  padding: 0 50px;
+  background-color: rgba(0, 0, 0, .4);
+  font-size: 20px;
+  font-weight: bold;
+  color: #ffffff;
+  margin-right: 20px;
+  transition: box-shadow 200ms ease-in-out;
+  cursor: pointer;
+
+  span {
+    display: inline-block;
+    padding: 14px 0 14px 10px;
+  }
+
+  :last-child {
+    margin-right: 0;
+  }
+
+  :hover {
+    background-color: #e7e7e7;
+    color: #000000;
+    box-shadow: 0 10px 20px 0 #000000;
+  }
+`;
+
+export const Overview = styled.p`
+  font-size: 25px;
+  width: 40%;
+  letter-spacing: .8px;
+  line-height: 50px;
+  /* _line-clamp */
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  cursor: default;
+
+  &::selection {
+    background-color: transparent;
+  }
+`;
