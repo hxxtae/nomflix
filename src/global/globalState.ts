a7d323f5b2ac7e536c5dcfdf985d8fe5bfb558df
hxxtae <fkdlxmfkdl@gmail.com>
2023-11-17 00:13:14 +0900

    :books: [Docs]: Add global state of mylist
    - Add global state of mylist data for import localStorage data

--- src/global/globalState.ts
+++ src/global/globalState.ts
@@ -1,8 +1,14 @@
 import { atom } from 'recoil';
 import { dto } from '../apis';
 import { initContentData } from '../constants';
+import { getContentsStorage } from '../utils';
 
 export const atomOfContentData = atom<dto.IContentData>({
   key: 'bannerState',
   default: { ...initContentData }
 });
+
+export const atomOfMylistData = atom<Map<number, dto.IContentData>>({
+  key: 'mylistState',
+  default: getContentsStorage('mylist')
+})
