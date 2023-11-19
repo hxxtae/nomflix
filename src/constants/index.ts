4d2b0c894a11db0f8930a271659ba218290252dd
hxxtae <fkdlxmfkdl@gmail.com>
2023-11-19 23:41:01 +0900

    :book: [feat]: Add genres.ts file in constants
    - add contentGenres object

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { MovieCategory, TvCategory, queryKey, queryKinds, mediaScreenSize } from './constants';
 export { initContentData } from './inits';
 export { darkTheme } from './theme';
+export { contentGenres } from './genres';
